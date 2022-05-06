706f23f1abe3cb210efe64ffd072180f2a8baaa4
Hady Mohamed <hadymohamed@pursuit.org>
2022-05-05 22:39:03 -0400

    created Home route and initial Navbar route

--- src/App.js
+++ src/App.js
@@ -1,39 +1,22 @@
-import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { apiURL } from "./util/apiURL";
-import axios from "axios";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./layout/navbar/Navbar";
+
+import Home from "./pages/Home";
 
 import "./App.css";
 
-// const API_BASE = process.env.REACT_APP_API_SITE;
-const API_BASE = apiURL();
-
 function App() {
-  const [recipes, setRecipes] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get(`${API_BASE}/recipe`)
-      .then((res) => {
-        const { payload } = res.data;
-        setRecipes(payload);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-
   return (
     <div className="App">
-      <h1>Recipe Book</h1>
-      {recipes.map((recipe, index) => {
-        return (
-          <div key={index}>
-            <h2>{recipe.title}</h2>
-            <p>{recipe.description}</p>
-          </div>
-        );
-      })}
+      <Router>
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </main>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import { apiURL } from "../util/apiURL";
+import axios from "axios";
+
+// const API_BASE = process.env.REACT_APP_API_SITE;
+const API_BASE = apiURL();
+
+export default function Home() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${API_BASE}/recipe`)
+      .then((res) => {
+        const { payload } = res.data;
+        setRecipes(payload);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Recipe Book</h1>
+      {recipes.map((recipe, index) => {
+        return (
+          <div key={index}>
+            <h2>{recipe.title}</h2>
+            <p>{recipe.description}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
