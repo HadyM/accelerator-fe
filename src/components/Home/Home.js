c5dad487e9d277aff6ea804a40b7a143f6f9ca27
Hady Mohamed <hadymohamed@pursuit.org>
2022-08-23 11:25:22 -0400

    testing webhook

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import { apiURL } from "../../util/apiURL";
 import axios from "axios";
 
-
 const API_BASE = apiURL();
 
 export default function Home() {
@@ -32,7 +31,7 @@ export default function Home() {
           </div>
         );
       })}
+      <div>Testing Web Hook</div>
     </div>
-   
   );
 }
