6713c3e1194a471db9c56ab28576d4cb34254e7b
Hady Mohamed <hadymohamed@pursuit.org>
2022-10-10 17:47:18 -0400

    created PotsAndPans Component Files

---
+++ src/components/PotsAndPansCard/PotsAndPansCard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./PotsAndPansCard.scss"
+
+const PotsAndPansCard = () => {
+  return (
+    <div>PotsAndPansCard</div>
+  )
+}
+
+export default PotsAndPansCard
---
+++ src/components/PotsAndPansCards/PotsAndPansCards.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./PotsAndPansCards.scss"
+
+const PotsAndPansCards = () => {
+  return (
+    <div>PotsAndPansCards</div>
+  )
+}
+
+export default PotsAndPansCards
