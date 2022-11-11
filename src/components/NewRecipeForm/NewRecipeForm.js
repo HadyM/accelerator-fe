0e1694d6a11020bf10196d0ec43ea9190b3cd870
Hady Mohamed <hadymohamed@pursuit.org>
2022-11-10 21:39:55 -0500

    created NewRecipe pages and NewRecipeForm Component

---
+++ src/components/NewRecipeForm/NewRecipeForm.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./NewRecipeForm.scss"
+
+const NewRecipeForm = () => {
+  return (
+    <div>NewRecipeForm</div>
+  )
+}
+
+export default NewRecipeForm
---
+++ src/pages/NewRecipe/NewRecipe.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./NewRecipe.scss"
+
+const NewRecipe = () => {
+  return (
+    <div>NewRecipe</div>
+  )
+}
+
+export default NewRecipe
