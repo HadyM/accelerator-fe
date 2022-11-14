28f61bff00f8f416ce3d30ca5a0596635346a11a
Hady Mohamed <hadymohamed@pursuit.org>
2022-11-14 02:12:25 -0500

    styled different form components

--- src/components/NewRecipeForm/NewRecipeForm.js
+++ src/components/NewRecipeForm/NewRecipeForm.js
@@ -50,58 +50,72 @@ const NewRecipeForm = () => {
     <>
       <div className="NewFormDetails">
         <Form onSubmit={handleSubmit} className="Form">
-          <Form.Group>
-            <Form.Label>Recipe Title</Form.Label>
+          <Form.Group className="InputContainer">
+            <Form.Label className="InputTitle">Recipe Title</Form.Label>
             <Form.Control
               id="title"
               type="text"
+              className="InputBox"
               value={newRecipe.title}
               onChange={handleInput}
-              placeholder="Enter Recipe Title"
+              placeholder="Enter Recipe Title...."
               required
             />
+            <Form.Text className="InputText">
+              Please enter the title name
+            </Form.Text>
           </Form.Group>
-          <Form.Group>
-            <Form.Label>Recipe Image URL</Form.Label>
+          <Form.Group className="InputContainer">
+            <Form.Label className="InputTitle">Recipe Image URL</Form.Label>
             <Form.Control
               id="image"
               type="url"
+              className="InputBox"
               value={newRecipe.image}
               onChange={handleInput}
-              placeholder="Enter Recipe Image URL"
+              placeholder="Enter Recipe Image URL...."
               required
             />
+            <Form.Text className="InputText">
+              Please enter the image URL
+            </Form.Text>
           </Form.Group>
-          <Form.Group>
-            <Form.Label>Recipe Description</Form.Label>
+          <Form.Group className="InputContainer">
+            <Form.Label className="InputTitle">Recipe Description</Form.Label>
             <Form.Control
               id="description"
               type="text"
+              className="InputBox"
               value={newRecipe.description}
               onChange={handleInput}
-              placeholder="Enter Recipe Description"
+              placeholder="Enter Recipe Description...."
               required
             />
+            <Form.Text className="InputText">
+              Please enter a short description of the recipe
+            </Form.Text>
           </Form.Group>
-          <Form.Group>
-            <Form.Label>Recipe Time</Form.Label>
+          <Form.Group className="InputContainer">
+            <Form.Label className="InputTitle">Recipe Time</Form.Label>
             <Form.Control
               id="time"
               type="text"
+              className="InputBox"
               value={newRecipe.time}
               onChange={handleInput}
-              placeholder="Enter Recipe Time"
+              placeholder="Enter Recipe Time...."
               required
             />
-            <Form.Text className="text-muted">
-              Please enter the recipes time length example "5 Minutes"
+            <Form.Text className="InputText">
+              Please enter the recipes time length for example "5 Minutes"
             </Form.Text>
           </Form.Group>
-          <Form.Group>
-            <Form.Label>Recipe Cooking Style</Form.Label>
+          <Form.Group className="InputContainer">
+            <Form.Label className="InputTitle">Recipe Cooking Style</Form.Label>
             <Form.Select
               aria-label="Default select example"
               id="cookingstyle"
+              className="InputBox"
               value={newRecipe.cookingstyle}
               onChange={handleInput}
               required
@@ -120,45 +134,51 @@ const NewRecipeForm = () => {
               <option value="blanching">Blanching</option>
               <option value="sous vide">Sous Vide</option>
             </Form.Select>
-            <Form.Text className="text-muted">
-              Please enter one of the following styles
+            <Form.Text className="InputText">
+              Please select one of the following cooking styles
             </Form.Text>
           </Form.Group>
-          <Form.Group>
-            <Form.Label>Recipe Ingredients</Form.Label>
+          <Form.Group className="InputContainer">
+            <Form.Label className="InputTitle">Recipe Ingredients</Form.Label>
             <Form.Control
               id="ingredients"
               type="text"
+              className="InputBox"
               value={newRecipe.ingredients}
               onChange={handleInput}
-              placeholder="Enter Ingredient List"
+              placeholder="Enter Ingredient List...."
               required
             />
-            <Form.Text className="text-muted">
+            <Form.Text className="InputText">
               Please enter the list of ingredients separated by a comma and
-              space example "2 eggs, 4 cups sugar, 1/2 teaspoon vanilla"
+              space for example "2 eggs, 4 cups sugar, 1/2 teaspoon vanilla"
             </Form.Text>
           </Form.Group>
-          <Form.Group>
-            <Form.Label>Recipe Instructions</Form.Label>
+          <Form.Group className="InputContainer">
+            <Form.Label className="InputTitle">Recipe Instructions</Form.Label>
             <Form.Control
               id="instructions"
               type="text"
+              className="InputBox"
               value={newRecipe.instructions}
               onChange={handleInput}
-              placeholder="Enter Instruction Descriptions"
+              placeholder="Enter Instruction Description...."
               required
             />
-            <Form.Text className="text-muted">
+            <Form.Text className="InputText">
               Please enter the instructions separated by a period, comma, and
-              space, folowed by Step example "Step 1: preheat the oven., Step 2:
-              add ingredients to a baking pan., Step 3 : etc etc"
+              space, folowed by Step for example "Step 1: preheat the oven.,
+              Step 2: add ingredients to a baking pan., Step 3 : etc etc"
             </Form.Text>
           </Form.Group>
-          <Button variant="primary" type="submit">
+          <Button className="FormButton" variant="primary" type="submit">
             Create Recipe
           </Button>
-          <Button variant="primary" onClick={handleCancel}>
+          <Button
+            className="FormButton"
+            variant="primary"
+            onClick={handleCancel}
+          >
             Cancel
           </Button>
         </Form>
