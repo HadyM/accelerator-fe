fa0adf0c85d45ded5e7918e47ee7d7844afdf5d8
Hady Mohamed <hadymohamed@pursuit.org>
2022-11-14 00:26:41 -0500

    Added loader to new recipe page, centered on page and made h1 bigger

--- src/pages/NewRecipe/NewRecipe.js
+++ src/pages/NewRecipe/NewRecipe.js
@@ -1,14 +1,18 @@
 import React from "react";
+import Loader from "react-loaders";
 import NewRecipeForm from "../../components/NewRecipeForm/NewRecipeForm";
 
 import "./NewRecipe.scss";
 
 const NewRecipe = () => {
   return (
-    <div className="NewForm">
-      <h1>Add A Delicious Recipe</h1>
-      <NewRecipeForm />
-    </div>
+    <>
+      <div className="NewForm">
+        <h1>Add A Delicious Recipe</h1>
+        <NewRecipeForm />
+      </div>
+      <Loader type="ball-scale-multiple" />
+    </>
   );
 };
 
